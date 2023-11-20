cd1c5d78879dba3d4cc28b78963dfd5162585d43
TalonEzio <vuthemanh1707@gmail.com>
2023-11-20 17:52:02 +0700

    Fix

--- De02/Program.cs
+++ De02/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 using System.Threading;
 
 namespace De02
@@ -10,12 +11,18 @@ namespace De02
         static readonly object LockObject = new object();
         private static bool _isStopTimer1 = false, _isStopTimer2 = false;
         private static Timer _timer1, _timer2;
+        private static readonly CountdownEvent CountdownEvent = new CountdownEvent(2);
         static void Main(string[] args)
         {
+            Console.InputEncoding = Console.OutputEncoding = Encoding.Unicode;
+
             _timer1 = new Timer(Timer1, null, 0, 5);
             _timer2 = new Timer(Timer2, null, 0, 10);
 
-            Thread.Sleep(10000000);
+            CountdownEvent.Wait();
+
+            Console.WriteLine("Bấm phím bất kì để kết thúc chương trình");
+            Console.ReadLine();
         }
 
         static void Timer1(object state)
@@ -25,17 +32,21 @@ namespace De02
             {
                 if (_isStopTimer1)
                 {
-                    Console.WriteLine("Timer1 is stopped");
                     return;
                 }
 
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
                 Console.Write("Timer1: s = ");
                 _s = Console.ReadLine();
+
+                Console.ResetColor();
+
                 if (_s != null && _s.Equals("thoat"))
                 {
                     _isStopTimer1 = true;
+                    CountdownEvent.Signal();
                     _timer1.Dispose();
-
                 }
             }
         }
@@ -47,15 +58,21 @@ namespace De02
             {
                 if (_isStopTimer2)
                 {
-                    Console.WriteLine("Timer2 is stopped");
+                    //Console.WriteLine("Timer2 is stopped");
                     return;
                 }
+
+                if (String.IsNullOrEmpty(_s)) return;
+
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"Timer2: s = {_s}");
-                if (_s != null) Console.WriteLine($"Timer2: reverse s = {String.Concat(_s.Reverse())}");
-                if (_s != null && _s.ToLower().Equals("thoat"))
+                Console.WriteLine($"Timer2: reverse s = {String.Concat(_s.Reverse())}");
+                Console.ResetColor();
+
+                if (_s.ToLower().Equals("thoat"))
                 {
                     _isStopTimer2 = true;
-
+                    CountdownEvent.Signal();
                     _timer2.Dispose();
 
                 }
--- De03/Program.cs
+++ De03/Program.cs
@@ -8,32 +8,40 @@ namespace De03
     {
         private static string _c;
         static readonly object LockObject = new object();
-        private static bool _isStopTimer1 = false, _isStopTimer2 = false;
+        private static bool _isStopTimer1, _isStopTimer2;
         private static Timer _timer1, _timer2;
+        private static readonly CountdownEvent CountdownEvent = new CountdownEvent(2);
         static void Main(string[] args)
         {
+
             Console.InputEncoding = Console.OutputEncoding = Encoding.Unicode;
             _timer1 = new Timer(Timer1, null, 0, 5);
             _timer2 = new Timer(Timer2, null, 10, 10);
-            Thread.Sleep(10000000);
+
+            CountdownEvent.Wait();
+
+            Console.WriteLine("Bấm phím bất kì để kết thúc chương trình");
+            Console.ReadLine();
         }
         static void Timer1(object state)
         {
             lock (LockObject)
             {
-                if (_isStopTimer1)
-                {
-                    Console.WriteLine("Timer1 is stopped");
-                    return;
-                }
+                if (_isStopTimer1) return;
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
 
                 Console.Write("Timer1: c = ");
-                _c = Console.ReadLine();
-                if (_c != null && _c.Equals("thoat"))
+                _c = Console.ReadLine() ?? "";
+
+                Console.ResetColor();
+
+
+                if (_c.ToLower().Equals("thoat") || String.IsNullOrEmpty(_c))
                 {
                     _isStopTimer1 = true;
+                    CountdownEvent.Signal();
                     _timer1.Dispose();
-
                 }
             }
         }
@@ -41,20 +49,22 @@ namespace De03
         {
             lock (LockObject)
             {
-                if (_isStopTimer2)
-                {
-                    Console.WriteLine("Timer2 is stopped");
-                    return;
-                }
-                Console.WriteLine($"Timer2: c = {_c}");
-                if (_c != null) Console.WriteLine($"Timer2: Số từ: {_c.Split(' ').Length}");
-                if (_c != null && _c.ToLower().Equals("thoat"))
+                if (_isStopTimer2) return;
+                if (_c.ToLower().Equals("thoat") || String.IsNullOrEmpty(_c))
                 {
                     _isStopTimer2 = true;
-
+                    CountdownEvent.Signal();
                     _timer2.Dispose();
-
+                    return;
                 }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine($"Timer2: c = {_c}");
+                Console.WriteLine($"Timer2: Số từ: {_c.Split(' ').Length}");
+
+                Console.ResetColor();
+
 
             }
         }
